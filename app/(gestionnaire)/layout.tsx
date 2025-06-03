64b19cfbe0779820a313fcc3f8781f855ecd3314
adandeigor <adandehustler@gmail.com>
2025-06-03 11:02:48 +0100

    correction

--- app/(gestionnaire)/layout.tsx
+++ app/(gestionnaire)/layout.tsx
@@ -1,15 +1,18 @@
-import Loader from "@/components/Loader"
-import { SidebarNavigation } from "./gestionnaire/components/Navigation"
+import Loader from "@/components/Loader";
+import { SidebarNavigation } from "./gestionnaire/components/Navigation";
+import { Suspense } from "react";
 import "leaflet/dist/leaflet.css"; // Ajout des styles Leaflet
-const gestionnaireLayout = ({children}:{children:React.ReactNode})=>{
-    return (<>
-        <div className="">
-            <SidebarNavigation  />
-            <Loader/>
-            <div className="max-w-screen-xl mx-auto w-full">
-                {children}
-            </div>
+const gestionnaireLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <div className="">
+        <SidebarNavigation />
+        <Loader />
+        <div className="max-w-screen-xl mx-auto w-full">
+          <Suspense fallback={<div>Chargement...</div>}>{children}</Suspense>
         </div>
-    </>)
-}
-export default gestionnaireLayout
+      </div>
+    </>
+  );
+};
+export default gestionnaireLayout;
