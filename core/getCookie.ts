33cb71e31fcd2e6099bb9e71387dbaf80792b66a
adandeigor <adandehustler@gmail.com>
2025-06-03 11:28:20 +0100

    fix getcookie

--- core/getCookie.ts
+++ core/getCookie.ts
@@ -1,7 +1,9 @@
-function getCookie(name: string): string | undefined {
+export default function getCookie(name: string) {
+  if (typeof document === "undefined") {
+    return null; // Return null or a default value during SSR
+  }
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()?.split(';').shift();
+  if (parts.length === 2) return parts.pop()?.split(";").shift() || null;
+  return null;
 }
-
-export default getCookie;
