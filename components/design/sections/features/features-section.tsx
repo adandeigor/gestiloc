ce5957d9444f976569211a775faf296185a99c36
adande igor <adandehustler@gmail.com>
2025-06-04 06:43:43 +0100

    nothing

--- components/design/sections/features/features-section.tsx
+++ components/design/sections/features/features-section.tsx
@@ -12,7 +12,7 @@ const FeaturesSection = () => {
             className="bg-white"
         >
             <div className="max-w-screen-xl mx-auto py-16">
-                <p className="text-3xl font-bold text-center">Simplifiez votre gestion locative</p>
+                <p className="text-3xl font-bold font-sans text-center">Simplifiez votre gestion locative</p>
                 <p className="text-lg text-center font-mono text-gray-600">Notre plateforme vous offre tous les outils nécessaires pour gérer efficacement vos biens immobiliers.</p>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-8 center px-4">
                     {
--- components/design/sections/welcome-section.tsx
+++ components/design/sections/welcome-section.tsx
@@ -23,8 +23,8 @@ const WelcomeSection = ()=> {
             <div className="absolute inset-0 bg-gradient-to-r from-primary to-transparent"></div>
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div className="relative z-10  md:text-start lg:max-w-2xl">
-                    <p className="mb-4 text-2xl tracking-tight font-bold md:font-extrabold text-white md:text-5xl lg:text-6xl">Gérez vos biens immobiliers en toute simplicité</p>
-                    <p className="mb-4 text-md font-semibold text-white md:text-lg lg:text-xl">GestiLoc vous offre une solution complète pour la gestion de vos propriétés locatives, du suivi des paiements à la communication avec vos locataires.</p>
+                    <p className="mb-4 text-2xl tracking-tight font-bold md:font-extrabold text-white md:text-5xl lg:text-6xl font-sans">Gérez vos biens immobiliers en toute simplicité</p>
+                    <p className="mb-4 text-md font-semibold text-white md:text-lg lg:text-xl font-mono">GestiLoc vous offre une solution complète pour la gestion de vos propriétés locatives, du suivi des paiements à la communication avec vos locataires.</p>
                     <div className="flex flex-col gap-4 md:flex-row md:gap-6 lg:gap-8">
                         <button className="px-10 py-4 rounded font-semibold text-sm text-white bg-accent hover:bg-accent/10 focus:ring-4 focus:ring-accent/50 transition-colors cursor-pointer duration-300 ease-in-out">
                             Commencer maintenant
